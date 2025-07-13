a61fc62d2673954999615893d9f50b18cdbd7b23
hws <hwonsuk11@gmail.com>
2025-07-14 05:56:06 +0900

    [1 회차 ] CS  기초   과제

--- 1(2)-CS_basics/script.sh
+++ 1(2)-CS_basics/script.sh
@@ -1,10 +1,22 @@
 
 # anaconda(또는 miniconda)가 존재하지 않을 경우 설치해주세요!
 ## TODO
+if ! command -v conda &> /dev/null; then
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+    bash miniconda.sh -b -p $HOME/miniconda
+    export PATH="$HOME/miniconda/bin:$PATH"
+    source "$HOME/miniconda/etc/profile.d/conda.sh"
+else
+    source "$(conda info --base)/etc/profile.d/conda.sh"
+fi
 
 
 # Conda 환셩 생성 및 활성화
 ## TODO
+if ! conda info --envs | grep -q '^myenv'; then
+    conda create -y -n myenv python=3.10
+fi
+conda activate myenv
 
 ## 건드리지 마세요! ##
 python_env=$(python -c "import sys; print(sys.prefix)")
@@ -17,20 +29,33 @@ fi
 
 # 필요한 패키지 설치
 ## TODO
+pip install mypy
 
 # Submission 폴더 파일 실행
-cd submission || { echo "[INFO] submission 디렉토리로 이동 실패"; exit 1; }
+cd submission 
 
 for file in *.py; do
     ## TODO
+    filename=$(basename "$file" .py)
+    input_file="../input/${filename}_input"
+    output_file="../output/${filename}_output"
 
+    if [[ -f "$input_file" ]]; then
+        python "$file" < "$input_file" > "$output_file"
+    fi
 done
 
 # mypy 테스트 실행 및 mypy_log.txt 저장
 ## TODO
+cd ..
+mypy ./submission/*.py > mypy_log.txt
 
 # conda.yml 파일 생성
 ## TODO
+conda env export > conda.yml
+
 
 # 가상환경 비활성화
 ## TODO
+conda deactivate
+
---
+++ 1(2)-CS_basics/submission/10830.py
@@ -0,0 +1,118 @@
+from __future__ import annotations
+import copy
+
+
+"""
+TODO:
+- __setitem__ 구현하기
+- __pow__ 구현하기 (__matmul__을 활용해봅시다)
+- __repr__ 구현하기
+"""
+
+
+class Matrix:
+    MOD = 1000
+
+    def __init__(self, matrix: list[list[int]]) -> None:
+        self.matrix = matrix
+
+    @staticmethod
+    def full(n: int, shape: tuple[int, int]) -> Matrix:
+        return Matrix([[n] * shape[1] for _ in range(shape[0])])
+
+    @staticmethod
+    def zeros(shape: tuple[int, int]) -> Matrix:
+        return Matrix.full(0, shape)
+
+    @staticmethod
+    def ones(shape: tuple[int, int]) -> Matrix:
+        return Matrix.full(1, shape)
+
+    @staticmethod
+    def eye(n: int) -> Matrix:
+        matrix = Matrix.zeros((n, n))
+        for i in range(n):
+            matrix[i, i] = 1
+        return matrix
+
+    @property
+    def shape(self) -> tuple[int, int]:
+        return (len(self.matrix), len(self.matrix[0]))
+
+    def clone(self) -> Matrix:
+        return Matrix(copy.deepcopy(self.matrix))
+
+    def __getitem__(self, key: tuple[int, int]) -> int:
+        return self.matrix[key[0]][key[1]]
+
+    def __setitem__(self, key: tuple[int, int], value: int) -> None:
+        self.matrix[key[0]][key[1]] = value % self.MOD
+        # 구현하세요!
+        pass
+
+    def __matmul__(self, matrix: Matrix) -> Matrix:
+        x, m = self.shape
+        m1, y = matrix.shape
+        assert m == m1
+
+        result = self.zeros((x, y))
+
+        for i in range(x):
+            for j in range(y):
+                for k in range(m):
+                    result[i, j] += self[i, k] * matrix[k, j]
+
+        return result
+
+    def __pow__(self, n: int) -> Matrix:
+        '''
+        n이 줄어드는 거랑 정확히 반대 연산으로 행렬곱 쌓기
+        '''
+        x, y = self.shape
+        assert x == y
+
+        result = self.eye(x)
+        s = self.clone()
+
+        while True:
+            if n == 0:
+                return result
+            elif n % 2 == 1:
+                result = result @ s
+                n -= 1
+            s = s @ s
+            n //= 2
+
+        pass
+
+    def __repr__(self) -> str:
+        return "\n".join(" ".join(str(i % self.MOD) for i in j) for j in self.matrix)
+        # 구현하세요!
+        pass
+
+
+from typing import Callable
+import sys
+
+
+"""
+-아무것도 수정하지 마세요!
+"""
+
+
+def main() -> None:
+    intify: Callable[[str], list[int]] = lambda l: [*map(int, l.split())]
+
+    lines: list[str] = sys.stdin.readlines()
+
+    N, B = intify(lines[0])
+    matrix: list[list[int]] = [*map(intify, lines[1:])]
+
+    Matrix.MOD = 1000
+    modmat = Matrix(matrix)
+
+    print(modmat ** B)
+
+
+if __name__ == "__main__":
+    main()
